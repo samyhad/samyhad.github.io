fb24fa0d53ed052407ac18cb37c4c6fced7b71a5
samyhad <60987282+samyhad@users.noreply.github.com>
2026-05-26 23:34:30 +0000

    Deploying to gh-pages from @ samyhad/samyhad.github.io@d0d274f137a8a1d24e219d2a833b37979e09c83f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "cv",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
